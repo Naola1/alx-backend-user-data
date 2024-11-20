5826f62b856794184cc1c0ba4ca88b25c5b0e717
naola1 <naolmitiku@gmail.com>
2024-11-20 16:05:33 +0300

    task-01

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+BD class
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
